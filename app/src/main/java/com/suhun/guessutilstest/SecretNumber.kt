cf15eeba909d63745b519aa692dcbb5d6025af27
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-03-25 11:47:51 +0800

    Add SecretNumber class and logic for SecretNumber.kt

---
+++ app/src/main/java/com/suhun/guessutilstest/SecretNumber.kt
@@ -0,0 +1,37 @@
+package com.suhun.guessutilstest
+
+import android.content.res.Resources
+import android.util.Log
+import java.util.Random
+
+class SecretNumber {
+    val tag:String = SecretNumber::class.java.simpleName
+    var secretRandom:Int = 0
+    var guessCounter:Int = 0
+    init{
+        secretRandom = Random().nextInt(100) + 1
+        Log.d(tag, "Secret number is $secretRandom")
+    }
+
+    fun verify(userInput:Int) = secretRandom - userInput
+
+    fun verifyResult(r:Resources, userInput:Int):String{
+        if(verify(userInput) > 0){
+            return r.getString(R.string.bigger)
+        }else if(verify(userInput) < 0){
+            return r.getString(R.string.smaller)
+        }else{
+            if(guessCounter < 3){
+                return r.getString(R.string.excellent) + secretRandom.toString()
+            }else{
+                return r.getString(R.string.you_got_it)
+            }
+        }
+    }
+
+    fun resetAll(){
+        guessCounter = 0
+        secretRandom = Random().nextInt(100) + 1
+        Log.d(tag, "Reset secret number is $secretRandom")
+    }
+}
